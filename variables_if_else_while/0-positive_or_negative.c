8f72b5ec2c793a45fc49a186ac974e9929b1f421
Rmxpvl <remy.pinville@gmail.com>
2026-02-24 09:18:17 +0000

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+/** 
+ * main - Set random number and say positive of not
+ * Return: 0 if the program run
+ */
 
-/* betty style doc for function main goes there */
 int main(void)
 {
 int n;
