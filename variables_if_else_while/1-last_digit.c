d6ace21b0b7ef5b51cc4afa3b09a06b412e67d41
Rmxpvl <remy.pinville@gmail.com>
2026-02-24 11:08:19 +0000

    1-last_digit.c

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/** main - Program that look for last digit number and determine if 0 or greater than x or equal
+ * Return: 0 if the program run
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+if (last_digit ==0)
+{
+printf("Last digit of %d is %d and is 0\n", n, last digit);
+}
+else if (last_digit > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, last digit);
+}
+else
+{
+printf("Last digit of %d is %d and is less than - and not 0\n", n, last digit);
+}
+return (0);
+}
