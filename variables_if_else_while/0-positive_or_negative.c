b1bb65a351440c3a934c00b49a42cddd9a4e314c
Rmxpvl <remy.pinville@gmail.com>
2026-02-24 09:12:46 +0000

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
@@ -12,7 +13,7 @@ n = rand() - RAND_MAX / 2;
 
 if (n > 0)
 {
-printf("%d  is positive\n", n);
+printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
