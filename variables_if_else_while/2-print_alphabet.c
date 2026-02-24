5a615fedce07a197048f7095624d7385f25246c0
Rmxpvl <remy.pinville@gmail.com>
2026-02-24 22:34:58 +0100

    Ajout 2 print

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -2,14 +2,23 @@
 #include <stdlib.h>
 #include <time.h>
 
+/**
+ * main - prints the alphabet in lowercase followed by a new line
+ *
+ * Return: Always 0
+ */
+
 int main(void)
 {
+
 char c = 'a';
+
 while (c <= 'z')
 {
 putchar(c);
 c++;
 }
+
 putchar('\n');
 return (0);
 }
