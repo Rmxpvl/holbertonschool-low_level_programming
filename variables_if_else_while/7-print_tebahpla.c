d1387edf3e9c1c86e7fcb060de53ab63cd1968c6
Jtech99 <eugi202205@gmail.com>
2026-02-25 11:11:09 +0100

    7print

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints the alphabet in lowercase in reverse
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+
+char c = 'z';
+
+while (c <= 'a')
+{
+putchar(c);
+c--;
+}
+putchar('\n');
+return (0);
+}
