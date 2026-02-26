172fd6461e48951b02b6347db5d2150734ff706f
Jtech99 <eugi202205@gmail.com>
2026-02-26 10:50:31 +0100

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Program print alphabet lowercase x10
+ */
+
+void print_alphabet(void)
+{
+char c;
+int i;
+for (i = 0; i < 10; i++)
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
