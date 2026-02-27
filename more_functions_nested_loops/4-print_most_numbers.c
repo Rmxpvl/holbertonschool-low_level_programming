6c19785c8b44a0a03e986c73238d38ad1ba243b4
Jtech99 <eugi202205@gmail.com>
2026-02-27 16:25:07 +0100

    Update

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+char n = '0';
+while (n <= '9')
+
+if (n != 2 && n != 4)
+{
+_putchar(n);
+n++;
+}
+_putchar('\n');
+}
