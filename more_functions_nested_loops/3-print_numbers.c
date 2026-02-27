8815e3591b3740e6a543d8214b09d635d11496e5
Jtech99 <eugi202205@gmail.com>
2026-02-27 15:50:40 +0100

    Update

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the digits numbers of base 10 starting from 0
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+char n = '0';
+while (n <= '9')
+{
+putchar(n);
+n++;
+}
+print_numbers()
+putchar('\n');
+return (0);
+}
