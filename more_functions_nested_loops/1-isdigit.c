70607b6f4943feda3de854ce4858c89254327d96
Jtech99 <eugi202205@gmail.com>
2026-02-27 15:11:50 +0100

    Update

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isdigit - digit numbers
+ * @c: c is the checked number
+ * Return: 1 if uppercase, 0 if not
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	return (1);
+
+
+	return (0);
+}
