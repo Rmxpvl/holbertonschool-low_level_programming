e25fc413aca680a89c8629ce37a18eb734c432dd
Rmxpvl <remy.pinville@gmail.com>
2026-02-28 17:40:41 +0100

    5add

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14, 10 times
+ * return: void
+ */ 
+
+void more_numbers(void)
+{
+    int i, j;
+
+    i = 0;
+    while (i < 10)
+    {
+        j = 0;
+        while (j <= 14)
+        {
+            if (j >= 10)
+                _putchar('1');
+            _putchar('0' + j % 10);
+            j++;
+        }
+        _putchar('\n');
+        i++;
+    }
+}
