9cd52597fb10aecc1ce65d77b54495ce7ab0719b
Rmxpvl <remy.pinville@gmail.com>
2026-03-01 15:11:48 +0100

    10-print_triangles.c

--- more_functions_nested_loops/10-print_triangles.c
+++ more_functions_nested_loops/10-print_triangles.c
@@ -9,24 +9,24 @@
  */
 void print_triangle(int size)
 {
-    int i, j;
+int i, j;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
 
-    for (i = 1; i <= size; i++)
-    {
-        /* print spaces */
-        for (j = 1; j <= size - i; j++)
-            _putchar(' ');
+for (i = 1; i <= size; i++)
+{
+/* print spaces */
+for (j = 1; j <= size - i; j++)
+_putchar(' ');
 
-        /* print # */
-        for (j = 1; j <= i; j++)
-            _putchar('#');
+/* print # */
+for (j = 1; j <= i; j++)
+_putchar('#');
 
-        _putchar('\n');
-    }
+_putchar('\n');
+}
 }
