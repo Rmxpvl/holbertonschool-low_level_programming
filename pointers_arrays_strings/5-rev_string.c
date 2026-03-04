a037ca827abfdfdaa52e50f55b644fa11e0fb2ab
Rmxpvl <remy.pinville@gmail.com>
2026-03-04 12:30:03 +0100

    ajout 5 et update main

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void rev_string(char *s)
+{
+int len = 0; i;
+char tmp;
+
+while (s[len] != '\0')
+    len++;
+
+    for (i = 0; i < len / 2; i++)
+{
+    char tmp = s[i];
+    s[i] = s[len - 1 - i];
+    s[len - 1 - i] = tmp;
+}
+}
