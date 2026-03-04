c648859b3823783b3339fb193f2c0d703ec172fd
Rmxpvl <remy.pinville@gmail.com>
2026-03-04 20:36:28 +0100

    100-atoi.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: pointer to the string to be converted
+ * Return: The integer value represented by the string, or 0 if no number
+ */
+
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
+	int i = 0;
+	int started = 0; /* flag pour indiquer si la conversion a commencé */
+
+while (s[i] != '\0')
+{
+	if (s[i] == '-')
+		sign *= -1;
+	else if (s[i] == '+' && !started)
+	;
+	else if (s[i] >= '0' && s[i] <= '9')
+		result = result * 10 + s[i] - '0';
+		started = 1;
+]	else if (started)
+		break; /* arrêter la conversion si un caractère non numérique est rencontré après le début */
+	i++;
+}
+return (result * sign);
+}
