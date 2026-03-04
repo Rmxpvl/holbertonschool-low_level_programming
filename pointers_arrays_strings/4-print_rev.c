bd15ee5e2bb4a8497c4a0ca2034dc52696ad4a2a
Rmxpvl <remy.pinville@gmail.com>
2026-03-04 12:01:57 +0100

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ */
+
+void print_rev(char *s)
+{
+int len = 0;
+
+while (s[len] != '\0')
+len++;
+
+while (len > 0)
+{
+len--;
+_putchar(s[len]);
+}
+
+_putchar('\n');
+}
