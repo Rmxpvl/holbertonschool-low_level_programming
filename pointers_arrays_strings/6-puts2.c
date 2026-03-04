1daa6f1fee0ba009acbccb9df4411325d67b176f
Rmxpvl <remy.pinville@gmail.com>
2026-03-04 14:00:37 +0100

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,13 +7,10 @@
 
 void puts2(char *str)
 {
-int i = 0;
-
-while (str[i] != '\0')
+while (*str != '\0')
 {
-_putchar(str[i]);  /* affiche le caractère courant */
-i += 2;            /* passe au caractère suivant "tous les deux" */
+_putchar(*str);
+str += 2;
 }
-
-_putchar('\n');        /* saut de ligne final */
+_putchar('\n');
 }
