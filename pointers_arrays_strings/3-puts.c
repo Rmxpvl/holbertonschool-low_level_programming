36ba11889734159d8d2d230c8063c35de7b09e36
Rmxpvl <remy.pinville@gmail.com>
2026-03-04 11:20:09 +0100

    update main 3puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Prints a string followed by a new line
+ * @str: pointer to the string to be printed
+ * Description: This function takes a pointer to a string and
+ * prints the string to the standard output, followed by a new line.
+ */
+
+ void _puts(char *str);
+{
+    int i = 0;
+
+    while (str[i] != '\0')
+    {
+        _putchar(str[i]);
+        i++;
+    }
+
+    _putchar('\n');
+}
