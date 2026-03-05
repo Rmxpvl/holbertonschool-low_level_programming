361aac49ea7edd239751b735b65068d264c596c8
Jtech99 <eugi202205@gmail.com>
2026-03-05 14:30:40 +0100

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -19,7 +19,7 @@ int _strcmp(char *s1, char *s2)
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]); /* retourner la différence du premier caractère différent */
+			return (s1[i] - s2[i]);
 		i++;
 	}
 
