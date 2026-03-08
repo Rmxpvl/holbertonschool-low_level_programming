2e1642954ee1bd7d58c76fbdaf55ef87ba5ab567
Rmxpvl <remy.pinville@gmail.com>
2026-03-08 20:53:38 +0100

    ajout1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Print the number of command line arguments
+ * @argc: The number of command line arguments
+ * @argv: ignored parameter
+ * Description: This program prints the number of command line arguments.
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc - 1);
+return (0);
+}
