3200d9cb344c8bc7d948e4cf25b76fd9a672d384
Rmxpvl <remy.pinville@gmail.com>
2026-03-08 20:22:08 +0100

    update

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -4,10 +4,8 @@
  * afficher_nom_programme - Affiche le nom du programme
  * @argv: Tableau des arguments passés au programme
  * Description: Affiche le nom du fichier grace a argv et printant le array 0 qui contient le nom du programme
- * Return: 0 (succès)
  */
 void afficher_nom_programme(char *argv[])
 {
     printf("Nom du programme : %s\n", argv[0]);
-    return (0);
 }
