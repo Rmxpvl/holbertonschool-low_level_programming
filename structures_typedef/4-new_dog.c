1f2448a5892e2ba8757ddd8f430b48baf8cc6788
Rmxpvl <remy.pinville@gmail.com>
2026-03-16 14:55:57 +0100

    dog update

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,24 +1,29 @@
-#ifndef DOG_H
-#define DOG_H
-
+#include "dog.h"
+#include <stdlib.h>
 /**
- * struct dog - a structure that defines a dog
+ * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
+ *
+ * Return: pointer to the new dog, or NULL if it fails
  */
-struct dog
+int _strlen(char *s)
 {
-char *name;
-float age;
-char *owner;
-};
-
-/* typedef pour simplifier l’utilisation de struct dog */
-typedef struct dog dog_t;
-
-/* function prototypes */
-void init_dog(dog_t *d, char *name, float age, char *owner);
-void print_dog(dog_t *d);
-
-#endif
+int len = 0;
+while (s[i])
+i++;
+return (i);
+}
+char *_strcpy(char *dest, char *src)
+{int i = 0;
+while (src[i])
+i++;
+while (src[i])
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}
