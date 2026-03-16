2f9009c5fe5b8048ecd01ba33c248da5b3884bd9
Rmxpvl <remy.pinville@gmail.com>
2026-03-16 14:41:46 +0100

    update

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "dog.h"
 
 /**
@@ -6,6 +8,7 @@
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
+ *
  * Return: pointer to new dog (dog_t), or NULL if failure
  */
 dog_t *new_dog(char *name, float age, char *owner)
