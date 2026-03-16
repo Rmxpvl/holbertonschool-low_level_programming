b34681384cb1fa0a36cbded5948c5f12b2e8ea1d
Rmxpvl <remy.pinville@gmail.com>
2026-03-16 10:02:58 +0100

    update commentaires

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "dog.h"
-
+/**
+ * struct dog - a structure that defines a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
 struct dog
 {
 char *name;
