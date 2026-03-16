df78f2adad434ef4bb0f4fa595315f63a9696038
Rmxpvl <remy.pinville@gmail.com>
2026-03-16 14:05:53 +0100

    dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,4 +16,7 @@ char *owner;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+struct dog *new_dog(char *name, float age, char *owner);
+void free_dog(struct dog *d);
+
 #endif
