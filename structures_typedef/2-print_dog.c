543e878e86c21d175978520816f445c1b696929f
Rmxpvl <remy.pinville@gmail.com>
2026-03-16 14:09:21 +0100

    ajout2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,10 +10,10 @@
  */
 void print_dog(struct dog *d)
 {
-    if (d == NULL)
-        return;
+if (d == NULL)
+return;
 
-    printf("%s\n", d->name ? d->name : "(nil)");
-    printf("%.6f\n", d->age);
-    printf("%s\n", d->owner ? d->owner : "(nil)");
+printf("%s\n", d->name ? d->name : "(nil)");
+printf("%.6f\n", d->age);
+printf("%s\n", d->owner ? d->owner : "(nil)");
 }
