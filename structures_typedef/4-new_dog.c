6eb404d36f8c3167bcd4ccf2fe036bf0ee66ac28
Rmxpvl <remy.pinville@gmail.com>
2026-03-16 14:53:02 +0100

    dog update

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,60 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "dog.h"
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * new_dog - creates a new dog
+ * struct dog - a structure that defines a dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
- *
- * Return: pointer to new dog (dog_t), or NULL if failure
  */
-dog_t *new_dog(char *name, float age, char *owner)
+struct dog
 {
-dog_t *d;
-char *name_copy;
-char *owner_copy;
+char *name;
+float age;
+char *owner;
+};
 
-/* allocate memory for struct dog */
-d = malloc(sizeof(dog_t));
-if (d == NULL)
-return (NULL);
+/* typedef pour simplifier l’utilisation de struct dog */
+typedef struct dog dog_t;
 
-/* allocate and copy name */
-if (name)
-{
-name_copy = malloc(strlen(name) + 1);
-if (name_copy == NULL)
-{
-free(d);
-return (NULL);
-}
-strcpy(name_copy, name);
-}
-else
-name_copy = NULL;
+/* function prototypes */
+void init_dog(dog_t *d, char *name, float age, char *owner);
+void print_dog(dog_t *d);
 
-/* allocate and copy owner */
-if (owner)
-{
-owner_copy = malloc(strlen(owner) + 1);
-if (owner_copy == NULL)
-{
-free(name_copy);
-free(d);
-return (NULL);
-}
-strcpy(owner_copy, owner);
-}
-else
-owner_copy = NULL;
-
-/* assign values */
-d->name = name_copy;
-d->age = age;
-d->owner = owner_copy;
-
-return (d);
-}
+#endif
