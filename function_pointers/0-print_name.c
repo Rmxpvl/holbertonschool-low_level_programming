83b1f1d5be86372b6df994fbaa4ea54357d699d8
Rmxpvl <remy.pinville@gmail.com>
2026-03-20 17:38:52 +0100

    ajout dossier

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - prints a name using a function pointer
+ * @name: name string to print
+ * @f: pointer to function used to print the name
+ *
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+    if (name != NULL && f != NULL)
+        f(name);
+}
