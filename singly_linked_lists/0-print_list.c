2ab2f72230f0ca6297c854e19aa8d177f42cb5ca
Rmxpvl <remy.pinville@gmail.com>
2026-03-27 17:48:44 +0100

    osef

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,18 +9,18 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t count;
+size_t count;
 
-    count = 0;
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%u] %s\n", h->len, h->str);
-        count++;
-        h = h->next;
-    }
+count = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
+count++;
+h = h->next;
+}
 
-    return (count);
+return (count);
 }
