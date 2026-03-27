bfc6a1066d781eb9fe09abf59463a887210225a3
Rmxpvl <remy.pinville@gmail.com>
2026-03-27 22:49:45 +0100

    AjoutRecursion

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,27 +1,17 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string followed by a new line
+ * _print_rev_recursion - prints a string in reverse
  * @s: string to print
  */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-int i = 0;
-
-while (s[i] != '\0')
-i++;
-
-i--; // se placer sur le dernier caractère
-
-while (i >= 0)
+if (*s == '\0')
 {
-printf("%c", s[i]);
-i--;
-}
+_putchar('\n');
+return;
 }
 
-int main(void)
-{
-_print_rev_recursion("hello");
-return 0;
+_print_rev_recursion(s + 1);
+_putchar(*s);
 }
