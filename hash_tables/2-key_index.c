28be6d9034bd00ba7888d058e12813f738b1d16b
Rmxpvl <remy.pinville@gmail.com>
2026-04-02 16:44:33 +0200

    hash

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,16 +1,27 @@
 #include "hash_tables.h"
 
 /**
- * key_index - gives the index of a key
- * @key: the key
- * @size: the size of the array of the hash table
+ * key_index - calcule l'indice d'une cle dans le tableau de hash
+ * @key: cle a convertir en indice
+ * @size: taille du tableau de la table de hash
  *
- * Return: index where the key/value pair should be stored
+ * Return: indice ou stocker la paire (key, value)
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-if (key == NULL || *key == '\0' || size == 0)
+/*
+* Verification defensive:
+* - key == 0: pointeur invalide
+* - *key == '\0': chaine vide, donc aucune cle exploitable
+* - size == 0: modulo impossible et tableau inexistant
+*/
+if (key == 0 || *key == '\0' || size == 0)
 return (0);
 
+/*
+* Etape 1: hash_djb2 transforme la chaine en grand entier.
+* Etape 2: % size ramene cette valeur dans l'intervalle [0, size - 1].
+* Cet indice correspond a la case du tableau ou placer/rechercher la cle.
+*/
 return (hash_djb2(key) % size);
 }
