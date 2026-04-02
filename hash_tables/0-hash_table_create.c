ec6da0d56cdd44b3719db22c93a25822c4e12909
Rmxpvl <remy.pinville@gmail.com>
2026-04-02 16:22:23 +0200

    hash

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include "hash_tables.h"
 
+./**
+ * hash_table_create - Creates a hash table
+ * @size: The size of the array
+ *
+ * Return: A pointer to the newly created hash table, or NULL if it failed
+ */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *ht;
