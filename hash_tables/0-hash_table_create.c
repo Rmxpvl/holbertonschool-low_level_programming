03674ba617a0f2580c9caf94733b304be75c8970
Rmxpvl <remy.pinville@gmail.com>
2026-04-02 16:19:28 +0200

    hash

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -3,31 +3,31 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht;
-    unsigned long int i;
+hash_table_t *ht;
+unsigned long int i;
 
-    if (size == 0)
-        return (NULL);
+if (size == 0)
+return (NULL);
 
-    /* 1. Allocation de la structure */
-    ht = malloc(sizeof(hash_table_t));
-    if (ht == NULL)
-        return (NULL);
+/* 1. Allocation de la structure */
+ht = malloc(sizeof(hash_table_t));
+if (ht == NULL)
+return (NULL);
 
-    /* 2. Initialisation de la taille */
-    ht->size = size;
+/* 2. Initialisation de la taille */
+ht->size = size;
 
-    /* 3. Allocation du tableau */
-    ht->array = malloc(sizeof(hash_node_t *) * size);
-    if (ht->array == NULL)
-    {
-        free(ht);
-        return (NULL);
-    }
+/* 3. Allocation du tableau */
+ht->array = malloc(sizeof(hash_node_t *) * size);
+if (ht->array == NULL)
+{
+free(ht);
+return (NULL);
+}
 
-    /* 4. Initialiser chaque case a NULL */
-    for (i = 0; i < size; i++)
-        ht->array[i] = NULL;
+/* 4. Initialiser chaque case a NULL */
+for (i = 0; i < size; i++)
+ht->array[i] = NULL;
 
-    return (ht);
+return (ht);
 }
