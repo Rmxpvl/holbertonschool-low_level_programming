6d9f3274ce659c44dbb70d6440e43a8314ad3195
Rmxpvl <remy.pinville@gmail.com>
2026-04-02 16:48:47 +0200

    hash

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -3,25 +3,20 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - adds an element to the hash table
- * @ht: hash table
- * @key: key (must not be empty)
- * @value: value associated with the key
+ * update_existing_key - update value if key already exists
+ * @head: first node of bucket list
+ * @key: key to match
+ * @value: new value to duplicate
  *
- * Return: 1 on success, 0 on failure
+ * Return: 1 if updated, 0 if key not found or allocation failure
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+static int update_existing_key(hash_node_t *head, const char *key,
+const char *value)
 {
-hash_node_t *node;
 hash_node_t *tmp;
 char *new_value;
-unsigned long int index;
 
-if (ht == 0 || key == 0 || *key == '\0' || value == 0)
-return (0);
-
-index = key_index((const unsigned char *)key, ht->size);
-tmp = ht->array[index];
+tmp = head;
 while (tmp)
 {
 if (strcmp(tmp->key, key) == 0)
@@ -35,6 +30,20 @@ return (1);
 }
 tmp = tmp->next;
 }
+
+return (0);
+}
+
+/**
+ * create_node - create a new hash node with duplicated key and value
+ * @key: key to duplicate
+ * @value: value to duplicate
+ *
+ * Return: pointer to new node, or 0 on failure
+ */
+static hash_node_t *create_node(const char *key, const char *value)
+{
+hash_node_t *node;
 
 node = malloc(sizeof(hash_node_t));
 if (node == 0)
@@ -54,6 +63,34 @@ free(node->key);
 free(node);
 return (0);
 }
+
+node->next = 0;
+return (node);
+}
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table
+ * @key: key (must not be empty)
+ * @value: value associated with the key
+ *
+ * Return: 1 on success, 0 on failure
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+hash_node_t *node;
+unsigned long int index;
+
+if (ht == 0 || key == 0 || *key == '\0' || value == 0)
+return (0);
+
+index = key_index((const unsigned char *)key, ht->size);
+if (update_existing_key(ht->array[index], key, value) == 1)
+return (1);
+
+node = create_node(key, value);
+if (node == 0)
+return (0);
 
 node->next = ht->array[index];
 ht->array[index] = node;
