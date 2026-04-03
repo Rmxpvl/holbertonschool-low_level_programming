abf76a0643c4b19c452ac465ddc5af2a7c872230
Rmxpvl <remy.pinville@gmail.com>
2026-04-03 18:02:35 +0200

    Addsecure

--- secure_data_handling/session.c
+++ secure_data_handling/session.c
@@ -53,9 +53,6 @@ int session_set_data(session_t *s, const unsigned char *data, size_t data_len)
 	if (!tmp)
 		return 0;
 
-if (!tmp)
-    return 0;
-
 s->data = tmp;
 	memcpy(s->data, data, data_len);
 	s->data_len = data_len;
